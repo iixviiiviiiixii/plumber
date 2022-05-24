df13aefdc03316039e432a198f9a6d4abffa31c9
iixviiiviiiixii <49006795+iixviiiviiiixii@users.noreply.github.com>
2022-05-25 00:20:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+
+
+
 $(function () {
-    shuffleElements($('.info'));
+    shuffleElements($('.top'));
 });
 
 function shuffleElements($elements) {
@@ -31,3 +34,45 @@ function shuffleElements($elements) {
         $parent.append($elements.eq(shuffled_array[i]));
     }
 }
+
+
+$(function () {
+    shuffleElements($('.info:not(.number)'));
+    $('.number').each(function (i) {
+        var number = i + 1;
+        $(this).append(number);
+    });
+});
+
+function shuffleElements($elements) {
+    var i, index1, index2, temp_val;
+
+    var count = $elements.length;
+    var $parent = $elements.parent();
+    var shuffled_array = [];
+
+
+    // populate array of indexes
+    for (i = 0; i < count; i++) {
+        shuffled_array.push(i);
+    }
+
+    // shuffle indexes
+    for (i = 0; i < count; i++) {
+        index1 = (Math.random() * count) | 0;
+        index2 = (Math.random() * count) | 0;
+
+        temp_val = shuffled_array[index1];
+        shuffled_array[index1] = shuffled_array[index2];
+        shuffled_array[index2] = temp_val;
+    }
+
+    // apply random order to elements
+    $elements.detach();
+    for (i = 0; i < count; i++) {
+        $parent.append($elements.eq(shuffled_array[i]));
+    }
+}
+
+
+
